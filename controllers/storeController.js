aa87f89b0f1c471f10398a530ea8b59a61aaf948
Abdelfatah <the_pilot.hezema@yahoo.com>
2020-10-09 11:48:22 +0200

    Querying database for stores and displaying them on the home page and the stores section

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,4 +14,10 @@ exports.createStore = async (req, res) => {
     const store = await (new Store(req.body).save());
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review!`)
     res.redirect(`/store/${store.slug}`)
+};
+
+exports.getStores = async (req, res) => {
+    // 1. query the database for all stores
+    const stores = await Store.find();
+    res.render('stores', {title: 'Stores', stores: stores})
 }
